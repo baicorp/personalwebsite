0d319de8fc7e9d2c1c6c8c468afe5d7d694dce1e
baicorp <bagusatoki@gmail.com>
2023-06-18 22:34:23 +0700

    integrate all the files in the components folder inside index.js

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import Footer from "./Footer";
+import Navbar from "./navbar";
+import Quotes from "./Quotes";
+import TechBadges from "./TechBadges";
+import ProjectCard from "./projectCard";
+import TechSkillsCard from "./TechSkillsCard";
+
+export { Navbar, Footer, Quotes, TechBadges, TechSkillsCard, ProjectCard };
